f7615c1214717d6b398be25b12896528b89aa752
Alumno <Alumno@PC17>
2019-09-24 09:42:10 -0500

    12

---
+++ One way/One way/Empleado.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace One_way
+{
+    class Empleado
+    {
+        public string Nombre { get; set; }
+        public string Titulo { get; set; }
+
+        public static Empleado GetEmpleado()
+        {
+            var emp = new Empleado()
+            {
+                Nombre = "Hector Cazadero", Titulo = "Todologo"
+            };
+            return emp;
+        }
+    }
+}
